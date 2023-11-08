526b8513528443c26491833387555f31b51e8aff
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-08 14:34:40 +0000

    day done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,23 +7,42 @@ function Main() {
 
     const deleteMovie = (movieId) => {
         const newList = moviesToDisplay.filter((elm) => elm.id !== movieId);
-        setMoviesToDisplay(newList)
+        setMoviesToDisplay(newList);
     };
+
+    let message = "";
+
+    /* if(moviesToDisplay.length) {
+        message = <h1>List of Movies</h1>
+    }
+    else {
+        message = <h1>Sorry, no movies to display</h1>
+    } */
+
+    moviesToDisplay.length ? (message = <h1>List of Movies</h1>) : (message = <h1>Sorry, no movies to display</h1>);
 
     return (
         <div>
-            <h1>This is the Main Component</h1>
+            {message}
 
             {moviesToDisplay.map((movieObj) => {
                 return (
                     <div key={movieObj.id} className="Main">
+                        {movieObj.imgURL ? (
+                            <img src={movieObj.imgURL} />
+                        ) : (
+                            <img src="https://dummyimage.com/182x268/ffffff/000000" />
+                        )}
+
                         <h2 className="card">
                             {movieObj.title} ({movieObj.year})
                         </h2>
                         <h3>Rating: {movieObj.rating}</h3>
+                        {movieObj.rating > 8 && <p>RECOMMENDED</p>}
                         {movieObj.genres.map((genre) => {
                             return <p>Genres: {genre}</p>;
                         })}
+                        
                         <button
                             onClick={() => {
                                 deleteMovie(movieObj.id);
@@ -31,6 +50,8 @@ function Main() {
                         >
                             Delete Movie
                         </button>
+                        <br></br>
+                        <br></br>
                     </div>
                 );
             })}
