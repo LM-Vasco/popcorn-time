8aa8855daf9b397615c950cd3b270b13dfadb3bf
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-08 13:47:15 +0000

    deleting movies

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,41 @@
 import movies from "../data/movies.json";
-import '../Main.css'
+import { useState } from "react";
+import "../Main.css";
 
 function Main() {
-  return (
-    <div>
-      <h1>This is the Main Component</h1>
-      {movies.map((movieObj) => {
-        return (
-          <div className="Main">
-            <h2 className="card">
-              {movieObj.title} ({movieObj.year})
-            </h2>
-            <h3>Rating: {movieObj.rating}</h3>
-            {movieObj.genres.map((genre) => {
-              return <p>Genres: {genre}</p>;
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+    const deleteMovie = (movieId) => {
+        const newList = moviesToDisplay.filter((elm) => elm.id !== movieId);
+        setMoviesToDisplay(newList)
+    };
+
+    return (
+        <div>
+            <h1>This is the Main Component</h1>
+
+            {moviesToDisplay.map((movieObj) => {
+                return (
+                    <div key={movieObj.id} className="Main">
+                        <h2 className="card">
+                            {movieObj.title} ({movieObj.year})
+                        </h2>
+                        <h3>Rating: {movieObj.rating}</h3>
+                        {movieObj.genres.map((genre) => {
+                            return <p>Genres: {genre}</p>;
+                        })}
+                        <button
+                            onClick={() => {
+                                deleteMovie(movieObj.id);
+                            }}
+                        >
+                            Delete Movie
+                        </button>
+                    </div>
+                );
             })}
-          </div>
-        );
-      })}
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Main;
