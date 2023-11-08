1a43f0ec9ea3756321ccae98dbe9e8b18b3e5d52
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-08 11:56:51 +0000

    map practice with css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,25 @@
+import movies from "../data/movies.json";
+import '../Main.css'
+
 function Main() {
-    return <h1>This is the Main Component</h1>
+  return (
+    <div>
+      <h1>This is the Main Component</h1>
+      {movies.map((movieObj) => {
+        return (
+          <div className="Main">
+            <h2 className="card">
+              {movieObj.title} ({movieObj.year})
+            </h2>
+            <h3>Rating: {movieObj.rating}</h3>
+            {movieObj.genres.map((genre) => {
+              return <p>Genres: {genre}</p>;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Main
+export default Main;
