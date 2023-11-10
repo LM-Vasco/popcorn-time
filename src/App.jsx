314d1158efd84f039393c4cb536f9e736b9bce2b
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-10 10:36:47 +0000

    Friday starting setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,37 @@
-import { useState } from "react";
+import { useState } from 'react';
 import movies from "./data/movies.json";
-import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import AddMovie from './components/AddMovie';
 
 function App() {
-    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
-    const [title, setTitle] = useState("");
-    const [rating, setRating] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
 
-    const deleteMovie = (movieTitle) => {
-        const newList = moviesToDisplay.filter((movieDetails) => {
-            return movieDetails.title !== movieTitle;
-        });
-        setMoviesToDisplay(newList);
-    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const addNewMovie = (newMovie) => {
+    const newList = [newMovie, ...moviesToDisplay];
+    setMoviesToDisplay(newList);
+  }
 
-        // create an object with the details of the new movie to add
-        const newMovie = {
-            title: title,
-            rating: rating,
-            imgURL: imageUrl,
-        };
+  const deleteMovie = (movieTitle) => {
+    const newList = moviesToDisplay.filter((movieDetails) => {
+      return movieDetails.title !== movieTitle;
+    });
+    setMoviesToDisplay(newList);
+  }
 
-        // update the list of movies
-        const newList = [newMovie, ...moviesToDisplay];
-        setMoviesToDisplay(newList);
 
-        // clear the form
-        setTitle("");
-        setRating("");
-        setImageUrl("");
-    };
-
-    return (
-        <>
-            <Header numberOfMovies={moviesToDisplay.length} />
-
-            <section className="box">
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Title:
-                        <input
-                            type="text"
-                            name="title"
-                            placeholder="enter the title"
-                            required={true}
-                            value={title}
-                            onChange={(e) => {
-                                setTitle(e.target.value);
-                            }}
-                        />
-                    </label>
-                    <label>
-                        Rating:
-                        <input
-                            type="number"
-                            name="rating"
-                            required={true}
-                            min={1}
-                            max={10}
-                            placeholder="enter the rating"
-                            value={rating}
-                            onChange={(e) => {
-                                setRating(e.target.value);
-                            }}
-                        />
-                    </label>
-
-                    <label>
-                        Image URL:
-                        <input
-                            type="url"
-                            name="imageUrl"
-                            placeholder="enter the image URL"
-                            value={imageUrl}
-                            onChange={(e) => {
-                                setImageUrl(e.target.value);
-                            }}
-                        />
-                    </label>
-
-                    <button>Create movie</button>
-                </form>
-            </section>
-
-            <Main moviesArr={moviesToDisplay} callbackToDelete={deleteMovie} />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <AddMovie callbackToAddMovie={addNewMovie} />
+      <Main moviesArr={moviesToDisplay} callbackToDelete={deleteMovie}  />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
-function Footer() {
-    return <h1>This is the Footer Component</h1>
+function Footer(){
+    return (
+        <h1>This is the Footer</h1>
+    );
 }
 
-export default Footer
+export default Footer;
