15e9f110d189ca92858ba47b96edab1d1a664682
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-10 13:05:29 +0000

    added id's to new movies

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,27 @@ function App() {
 
 
   const addNewMovie = (newMovie) => {
-    const newList = [newMovie, ...moviesToDisplay];
+    // find out id for the movie that we want to add
+    const movieIds = moviesToDisplay.map(function (elm) {
+      return elm.id;
+    });
+    const maxId = Math.max(...movieIds);
+    const nextId = maxId + 1;
+
+    // prepare an object with the details of the new movie (inc. the id)
+    const movieDetails = {
+      ...newMovie,
+      id: nextId
+    }
+
+    const newList = [movieDetails, ...moviesToDisplay];
     setMoviesToDisplay(newList);
   }
 
-  const deleteMovie = (movieTitle) => {
+  
+  const deleteMovie = (movieId) => {
     const newList = moviesToDisplay.filter((movieDetails) => {
-      return movieDetails.title !== movieTitle;
+      return movieDetails.id !== movieId;
     });
     setMoviesToDisplay(newList);
   }
